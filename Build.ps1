79c7b3bd4b4929aedd6fc22fcbb9a8db3e434204
Joseph Steele-Perkins <joseph_steeleperkins@yahoo.co.uk>
2019-02-01 10:09:42 +0000

    Changed Build.ps1 so uses control file

--- Build.ps1
+++ Build.ps1
@@ -1,27 +1,24 @@
-﻿param ([string]$BuildID='xxxxx', [string]$Rollback='False',[string]$Product,[string]$Test = 'True')
-
-#Set-Location $PSScriptRoot
+﻿param ([string]$BuildID='Multiple', [string]$Rollback='False',[string]$Product,[string]$Test = 'True',[string]$ControlFile)
 
     #change the location to the MarTech folder
 
+
+Set-Location $PSScriptRoot
 $pathdd = ""
 split-path $(get-location)|Set-Variable "pathdd"
-#write-host 'parent directory is ' $pathdd
 $RepoPath = $pathdd + '\Martech'
-#write-host 'repo directory is ' $RepoPath
-
 Set-Location $RepoPath
+    
     #enter the ID of the build/release. This will appear in all the scripts being run as part of this build.
 
-#$BuildID = "MAR-23"
-
-    #enter the environment
-
+#$BuildID = "Multiple"
+#$ControlFile = 'C:\MarTechControlFiles\Integration.txt'
 #$Product = "Diamond"
-#$Test = "False"
+#$Test = "True"
 #$Rollback = "False"
 
-    #The log file will be called Test, Build or Rollback based on the parameters
+
+    #The log file will start Test, Build or Rollback based on the parameters
 
 if ($Test -eq  'True')
 {$LogFileNameBit = 'TEST_'}
@@ -34,6 +31,7 @@ else
 {$LogFileNameBit2 = 'Build'}
 
     #set the name of the log file. This is the build/release ID puls the date and time. If this is a test run then amend the log file name
+    #if no buildID has been supplied then create it with the word Multiple to indicate there are more than one JiraID
 
 $LogFileName = 'Log_' + $LogFileNameBit + $LogFileNameBit2 + "_" + $BuildID + "_" +  $(get-date).ToString("yyyyMMdd_HHmmss") + ".txt"
 
@@ -50,9 +48,52 @@ $LogFileNameFull = "C:\MarTechLog\" + $LogFileName
 
 New-Item $LogFileNameFull | out-null 
 
-    #Create an empty hashtable. This will hold script path and run order
 
-$Scripts = @{}
+    #Create an array to hold the BuildsIDs to loop through
+
+$BuildIDs = @()
+
+    #Populate this array
+    #If the $BuildID is not its default value then put this in only
+
+    if($BuildID-ne'Multiple')
+    {
+
+    $BuildIDs += $BuildID
+
+    }
+    else
+
+    #otherwise get them by looping through the control file
+
+    {
+    Get-Content $ControlFile| ForEach-Object {
+        if($_ -match 'ItemID:')
+            {
+            $line = ""
+            $line = $_
+            $BuildIDs += $line.Substring(7)
+            }
+        }
+    }
+
+    #if count is 0 then create error and exist
+    if( $BuildIDs.Count -eq 0)
+    {
+    Add-Content $LogFileNameFull 'No Build IDs. Build aborted'
+    exit
+    }
+
+
+
+ #for each buildID do the following
+ 
+
+$BuildIDs | ForEach-Object { 
+  
+  $BuildID = $_
+
+  Add-Content $LogFileNameFull $BuildID
 
     #initialise the Errored variable to 0
 
@@ -60,10 +101,15 @@ $Errored = 0
 
     #"--" + $BuildID
 
+
+    #Create an empty hashtable to hold the scripts. This will hold script path and run order
+
+    $Scripts = @{}
+    
     #get every file that begins with the environment variable 
-
-
+    
 Get-ChildItem  -Filter $Product*.* | 
+
 
     #loop through each file
 
@@ -84,7 +130,8 @@ Foreach-Object {
         #$BuildID = 'Build00001'
 
     $content | Where-Object {$_ -match ("--" + $BuildID) -or $_ -eq "--Regression"} |  Set-Variable  "line" 
-
+        
+        
          
         #if the line variable is not empty (ie the file contains the release ID)...
 
@@ -92,7 +139,7 @@ Foreach-Object {
     {
 
         #then assign the path of the file to the Path variable
-
+        #write-host $_.FullName
     $Path = $_.FullName
 
         #and find the position of the * which indicates where the release order is. 
@@ -106,7 +153,8 @@ Foreach-Object {
         {
 
             #then get the number that follows the * and assign to the ReleaseOrder variable
-
+            #write-host $PositionOfReleaseOrder
+            #write-host $line
         $ReleaseOrder = [int]$line.Substring($PositionOfReleaseOrder+1,2)
         }
         else
@@ -123,11 +171,13 @@ Foreach-Object {
     }
 }
 
+
     #re-order the hash table by the "value" ie the release order 
 
 $Scripts = $Scripts.GetEnumerator() | sort -Property value
 
     #for each row in the hash table...
+
 
 ForEach ($d in $Scripts)
 {
@@ -137,7 +187,6 @@ try
 
     #Write-Host $d.Key
     #try to run the contents of the file against the local database. If Rollback is true then only run the rollback script
-
     if (($Rollback -eq 'True' -and $d.Key -like '*rollback*') -or ($Rollback -eq 'False' -and $d.Key -notlike '*rollback*'))
     {
         if ($Test-eq 'True')
@@ -159,7 +208,6 @@ try
                 if ($FileExtension -eq 'sql')
                     {
                     $StringArray = "BuildID='$BuildID '"
-                    Write-Host $StringArray
                     Invoke-Sqlcmd -InputFile $d.Key -ServerInstance $InstanceName -ErrorAction Stop -Variable $StringArray
                      }
                 else
@@ -214,4 +262,5 @@ elseif ($QueryResultColumn -eq -1)
 {$TestResult = 'No test rows'}
 
 Add-Content $LogFileNameFull $TestResult
+}
 }
--- BuildFeature.bat
+++ BuildFeature.bat
@@ -1,6 +1,12 @@
 REM what needs to be restored?
 
+Powershell.exe -executionpolicy remotesigned -file RestoreFromTo.ps1 -FromEnvironment  DESKTOP-CGRB0T0\LIVE -ToEnvironment DESKTOP-CGRB0T0
+
 REM what branch needs to be pulled?
 
+REM - n/a
+
 REM what IDs need to be built?
-Powershell.exe -executionpolicy remotesigned -file Build.ps1 -BuildID MAR101 -Environment Diamond -Test false -Rollback false
+Powershell.exe -executionpolicy remotesigned -file Build.ps1 -Environment Diamond -Test false -Rollback false -ControlFile C:\MarTechControlFiles\Integration.txt
+
+pause
